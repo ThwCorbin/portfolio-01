906a7f70ff128056a19eda107e43c9982c4fe3c9
Thomas Corbin <30200976+ThwCorbin@users.noreply.github.com>
2018-06-14 11:32:27 -0400

    Set up server to deploy via heroku

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
-const express = require("express");
-const app = express();
+// Built in Node.js modules
 const path = require("path");
 
+// Third party libraries
+const express = require("express");
+const app = express();
+// The app returned by express() is in fact a JavaScript Function, 
+// designed to be passed to Node’s HTTP servers as a callback to handle requests.
+
+// Local requirements
+// ...for this app, currently no local requirements
+
 const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
-     res.sendFile(`${publicPath}/index.html`);
+  res.sendFile(`${publicPath}/index.html`);
 });
 
-app.listen(3000);
+app.listen(port, () => console.log(`Server is up on port ${port}.`));
+// Binds and listens for connections on the specified host and port.
+// This method is identical to Node’s http.Server.listen().
